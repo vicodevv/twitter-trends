9b90520686ab3dbd8999e992a64ee24e91e22fd9
Victor Omoniyi <vomoniyi2001@gmail.com>
2022-07-07 10:57:21 +0100

    dropdown locations

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,31 @@
 import './App.css';
+import {FaCrosshairs} from 'react-icons/fa'
+import logo from './twitter.svg';
 
 function App() {
+  function handleLocation(){
+    alert("No Location selected")
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="logo" alt="twitter logo" />
+        <h2>Twitter Trends</h2>
       </header>
+      <div className="menu">
+        <select name="trending-places">
+          <option value="1"> Worldwide </option>
+          <option value="23424975">UK</option>
+          <option value="23424768">Brazil</option>
+          <option value="23424829">Germany</option>
+          <option value="23424900">Mexico</option>
+          <option value="23424775">Canada</option>
+          <option value="23424977">United States</option>
+        </select>
+        <div className="location" onClick={handleLocation}>
+          <FaCrosshairs />
+        </div>
+        </div>
     </div>
   );
 }
