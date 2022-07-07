ee9e5138eb5370d3b82ca4e9c85a4c9ff5f17d58
Victor Omoniyi <vomoniyi2001@gmail.com>
2022-07-07 11:11:48 +0100

    trends api

--- backend/routes/api.route.js
+++ backend/routes/api.route.js
@@ -1,10 +1,21 @@
 const router = require('express').Router();
-const twitter = require('twitter')
+const Twitter = require('twitter')
+
+const client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_API_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_API_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_ACCESS_SECRET,
+})
+
 
 // To get trending topics
 router.get('/trends', async (req, res, next) => {
   const id = req.query.woeid;
-  res.send({ message: 'Ok api is working 🚀' });
+  const trends = await client.get('trends/place.json', {
+    id: 1
+  })
+  res.send(trends);
 });
 
 // To get the WOE-ID for a particular location
